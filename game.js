3a555a861a7178b8188db98d8b8c7b8d38845c4a
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-24 10:47:34 +0100

    Create connections between HTML(index.html) and JS(game.js)

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ class Game {
         this.wallet = new Wallet(start);
         document.getElementById('start').addEventListener('click', this.startGame);
         this.spanWallet = document.querySelector('.panel span.wallet');
-        this.boards = document.querySelectorAll('div.cplors');
+        this.boards = [...document.querySelectorAll('div.color')];
         this.inputBid = document.getElementById('bid');
         this.spanResult = document.querySelector('.score span.result');
         this.spanGames = document.querySelector('.score span.number');
@@ -13,9 +13,25 @@ class Game {
 
         this.render();
     }
-    render(money = this.wallet.getWalletValue()) {
+    render(colors = ['#2ee', '#2ee', '#2ee'], money = this.wallet.getWalletValue(), result = "", stats = [0, 0, 0], bid = 0, wonMoney = 0) {
         // console.log("gramy!!!");
+
+        this.boards.forEach((board, i) => {
+            board.style.backgroundColor = colors[i];
+        });
+
+
         this.spanWallet.textContent = money;
+        if (result) {
+            result = `Wygrałeś ${wonMoney}`;
+        }
+        else if (!result && result != "") {
+            result = `Przegrałeś ${bid}`;
+        }
+        this.spanResult.textContent = result;
+        this.spanGames.textContent = stats[0];
+        this.spanWins.textContent = stats[0];
+        this.spanLosses.textContent = stats[0];
     }
 
     startGame() {
